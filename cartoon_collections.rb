b2b2965710a436e8082b64d5c53ffab5bbc9e4d8
Yasmine Hartung <y.hartung@gmail.com>
2019-03-12 12:02:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves (names)
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (names)
+  names.collect {|name|
+    name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
